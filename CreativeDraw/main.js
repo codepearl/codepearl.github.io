cbf6b5bcf573389c4911182d9346479e17858343
Pearl Park <blood_04@naver.com>
2023-05-23 21:29:16 +0900

    update change function

--- CreativeDraw/main.js
+++ CreativeDraw/main.js
@@ -225,4 +225,11 @@ function change()
 {
     console.log('change image');
     console.log(selected);
+    var img = document.createElement("img");
+    var src = 'img/' + selcted '.png';
+    img.src = src;
+    img.width = 1570;
+    img.height = 475;
+    img.alt = selected;
+    document.body.appendChild(img);
 }
