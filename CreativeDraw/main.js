7c003327dbc2686868853d0756d170a17aab65df
Pearl Park <blood_04@naver.com>
2023-05-23 22:08:46 +0900

    fix error

--- CreativeDraw/main.js
+++ CreativeDraw/main.js
@@ -257,5 +257,6 @@ function check()
     selected = candidate[selectedNum];
     console.log(selected);
     const element = document.getElementById('target');
-    element.innerHTML = '지금 그린것이 ' + selected + '가 맞나요?';
+    var str = '지금 그린것이 ' + selected + '가 맞나요?'
+    element.innerText = str;
 }
