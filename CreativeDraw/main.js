1f4dc162c478cce52129740f59480d04a977f81a
junyoung4279 <134495997+junyoung4279@users.noreply.github.com>
2023-06-01 19:45:45 +0900

    Update main.js

--- CreativeDraw/main.js
+++ CreativeDraw/main.js
@@ -380,17 +380,16 @@ function LoadTrans()
 	
 	koFile.onload = function (event) {
 		var koData = event.target.result; // 읽은 한국어 데이터
-		console.log(koData[0]);
-		for (var i = 0; i < 100; i++) {
-			trans_kr[enData[i]] = koData[i];
-			}
+		koFile.readAsText('translation_ko.txt');
 		};
 	
 	enFile.onload = function (event) {
 		var enData = event.target.result; // 읽은 영어 데이터
-		koFile.readAsText('translation_ko.txt'); // 한국어 파일 읽기 시작
+		enFile.readAsText('translation_en.txt'); // 영어 파일 읽기 시작
 		};
-	enFile.readAsText('translation_en.txt'); // 영어 파일 읽기 시작
+	for (var i = 0; i < 100; i++) {
+		trans_kr[enData[i]] = koData[i];
+		}
 }
 
 
