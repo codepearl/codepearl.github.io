5dab94d470b7d34603110c036ea948601f5a58c0
junyoung4279 <134495997+junyoung4279@users.noreply.github.com>
2023-06-01 21:44:14 +0900

    Update main.js

--- CreativeDraw/main.js
+++ CreativeDraw/main.js
@@ -379,20 +379,20 @@ function changeLanguage()
 function LoadTrans() 
 {    var ko = [];
      var en = [];
-    data=readTextFile("translation_en.txt");
-    const lst = data.split(/\n/);
+    var data=readTextFile("translation_en.txt");
+    var lst = data.split(/\n/);
     for (var i = 0; i < lst.length - 1; i++)
     {
-        let symbol = lst[i];
+        var symbol = lst[i];
         en[i] = symbol;
     }
  	console.log(en[0]);
  	console.log(en[1]);
-    data=readTextFile("translation_ko.txt");
-    const 1st = data.split(/\n/);
+     data=readTextFile("translation_ko.txt");
+     1st = data.split(/\n/);
     for (var i = 0; i < 1st.length - 1; i++)
     {
-        let symbol = 1st[i];
+        symbol = 1st[i];
         ko[i] = symbol;
     }
     for (var i=0; i<100; i++)
