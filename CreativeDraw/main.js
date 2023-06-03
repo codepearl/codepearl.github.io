5e285a68c4ff944acc967f0df59c9871c849f664
junyoung4279 <134495997+junyoung4279@users.noreply.github.com>
2023-06-03 22:05:11 +0900

    Update main.js

--- CreativeDraw/main.js
+++ CreativeDraw/main.js
@@ -227,7 +227,7 @@ async function start(cur_mode)
 function allowDrawing()
 {
     canvas.isDrawingMode = 1;
-    document.getElementById('status').innerHTML = <style=margin-left:550px;margin-top:-37px;>'AI가 그림을 다시 그려줍니다.';
+    document.getElementById('status').innerHTML = <style="margin-left:550px;margin-top:-37px;">'AI가 그림을 다시 그려줍니다.';
     document.getElementById('status').innerHTML = '그림을 그려주세요.';
     $('button').prop('disabled', false);
     var slider = document.getElementById('myRange');
